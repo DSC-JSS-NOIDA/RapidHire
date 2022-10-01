1828db0db4b04db6160988fd821fa7ee95df34a2
princegupta25 <princesmart.gupta@gmail.com>
2022-10-01 10:49:15 +0530

    initial

---
+++ app/src/main/java/com/example/rapidhire/activites/AuthActivity.kt
@@ -0,0 +1,15 @@
+package com.example.rapidhire.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.rapidhire.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+
+    }
+}
---
+++ app/src/main/java/com/example/rapidhire/fragments/RegisterFragment.kt
@@ -0,0 +1,37 @@
+package com.example.rapidhire.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.rapidhire.R
+
+
+class RegisterFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =inflater.inflate(R.layout.fragment_register, container, false)
+        val tvLogin = view.findViewById<TextView>(R.id.tv_signin)
+        tvLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_logInFragment)
+        }
+
+        return view
+    }
+
+
+
+}
