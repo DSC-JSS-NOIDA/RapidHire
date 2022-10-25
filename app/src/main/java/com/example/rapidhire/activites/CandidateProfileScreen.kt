b670c2d9898d6002e6bd954166d97cd7d85b6cb5
harshitkeshrihk <harshitkeshrihk@gmail.com>
2022-10-26 00:14:52 +0530

    Screens Added

---
+++ app/src/main/java/com/example/rapidhire/activites/CandidateProfileScreen.kt
@@ -0,0 +1,11 @@
+package com.example.rapidhire.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CandidateProfileScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_candidate_profile_screen)
+    }
+}
